0507d81cc941ae2f370dceb096fcadcf841c0256
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-05-13 19:16:23 -0400

    updates to note taking app

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded());
 const port = 3000
 
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" })
 
+//Endpoints to server the HTML
 app.get('/', (req, res) => {
   res.sendFile("public/index.html", {root:__dirname})
 })
@@ -13,6 +18,32 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/signup', (req, res) => {
+  res.sendFile('public/signup.html', {root:__dirname})
+})
+
+// Endpoints for APIs
+app.post('/getnotes', (req, res) => {
+  const {userToken } = req.body
+  res.sendFile('public/signup.html', {root:__dirname})
+})
+
+app.post('/login', (req, res) => {
+  const {userToken} = req.body
+  res.sendFile('public/signup.html', {root:__dirname})
+})
+
+app.post('/signup', (req, res) => {
+  const {userToken} = req.body
+  res.sendFile('public/signup.html', {root:__dirname})
+})
+
+app.post('/addnote', (req, res) => {
+  const {userToken} = req.body
+  res.sendFile('public/signup.html', {root:__dirname})
+})
+
+app.post('/deletenote', (req, res) => {
+  const {userToken} = req.body
   res.sendFile('public/signup.html', {root:__dirname})
 })
 
